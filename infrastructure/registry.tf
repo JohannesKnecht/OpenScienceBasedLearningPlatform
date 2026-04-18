f53fba1ce34ea18bbb35ebf28db179e2229c5c86
Johannes Laurenz Knecht <76184884+JohannesKnecht@users.noreply.github.com>
2026-04-18 19:12:08 +0200

    Update repository_id in registry.tf to use project variable

--- infrastructure/registry.tf
+++ infrastructure/registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "container_repo" {
   location      = "us-central1"
-  repository_id = "${random_id.default.hex}-repository"
+  repository_id = var.project + "-repository"
   description   = "docker repository"
   format        = "DOCKER"
 
