423ca18d4c3bf568cfa8e8ca463529f5085c5c31
Johannes Laurenz Knecht <76184884+JohannesKnecht@users.noreply.github.com>
2026-04-25 17:24:49 +0200

    feat: cleanup old images.

--- infrastructure/registry.tf
+++ infrastructure/registry.tf
@@ -4,6 +4,15 @@ resource "google_artifact_registry_repository" "container_repo" {
   description   = "docker repository"
   format        = "DOCKER"
 
+  # Keep the latest 10 versions per package/image.
+  cleanup_policies {
+    id     = "keep-latest-10"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 10
+    }
+  }
+
   docker_config {
     immutable_tags = false
   }
